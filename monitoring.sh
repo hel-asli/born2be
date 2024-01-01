4fb3dea7cab69b91faef267c05c492814ddebfcc
hel-asli <149835057+hel-asli@users.noreply.github.com>
2024-01-01 07:54:22 +0100

    monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 arch_kernel=$(uname -a)
 pcpu=$(nproc --all)
 vcpu=$(cat /proc/cpuinfo | grep ^processor | wc -l)
@@ -13,29 +14,21 @@ user_log=$(who -u | awk '{print $1}' | uniq| wc -l)
 sudo_use=$(journalctl _COMM=sudo | grep -c COMMAND)
 ip_addr=$(hostname -I)
 mac_addr=$(ip addr | grep -E '.*link/ether' | awk '{print $2}')
+conections_tcp=$(netstat -tn | grep tcp | grep ESTABLISHED | uniq | wc -l)
 cpu_load=$(iostat -c | grep -A 1 "idle" | grep "^ " | awk '{printf "%.2f%%\n", 100-$NF}')
 
-echo "arch : $arch_kernel"
-echo "vcpu : $vcpu"
-echo "pcpu : $pcpu"
-echo "free : $free_ram"
-echo "lvm status : $lvm_status"
-echo "lastboot : $last_boot"
-echo "usetlog : $user_log"
-echo "sudolog: $sudo_use"
-echo "Ip4 : $ip_addr ($mac_addr)"
-echo "cmd : $sudo_use"
-echo "cpu load $cpu_load"
-echo "used_disk $used/$total ($percent)"
-echo "arch : $arch_kernel"
-echo "vcpu : $vcpu"
-echo "pcpu : $pcpu"
-echo "free : $free_ram"
-echo "lvm status : $lvm_status"
-echo "lastboot : $last_boot"
-echo "usetlog : $user_log"
-echo "sudolog: $sudo_use"
-echo "Ip4 : $ip_addr ($mac_addr)"
-echo "cmd : $sudo_use"
-echo "cpu load $cpu_load"
-echo "used_disk $used/$total ($percent)"
+function ft_output(){
+	echo "#Architecture: $arch_kernel"
+	echo "#CPU physical : $pcpu"
+	echo "#vCPU : $vcpu"
+	echo "#Memory Usage: $free_ram"
+	echo "#Disk Usage: $used/$total ($percent)"
+	echo "#CPU load: $cpu_load"
+	echo "#Last boot: $last_boot"
+	echo "#LVM use: $lvm_status"
+	echo "#Connections TCP : $conections_tcp ESTABLISHED"
+	echo "#User log: $user_log"
+	echo "#Network: $ip_addr ($mac_addr)"
+	echo "#Sudo : $sudo_use cmd"
+}
+ft_output | wall
